79d651cdf3107c036260d213d4a05058c4b496e1
Michael Apfelbeck <m.apfelbeck@gmail.com>
2024-08-17 14:48:00 -0700

    Setup globel scene

--- Scripts/GeometryLoader.cs
+++ Scripts/GeometryLoader.cs
@@ -1,12 +1,20 @@
 using Godot;
 using System;
 
-public class TestTilesLoader : Node2D
+public class GeometryLoader : Node2D
 {
     [Export]
     public PackedScene geometry;
 
     public override void _Ready()
+    {
+        if (geometry != null)
+        {
+            Load(geometry);
+        }
+    }
+
+    public void Load(PackedScene geometry)
     {
         Node geo = geometry.Instance();
         AddChild(geo);
--- Scripts/TitleScreen.cs
+++ Scripts/TitleScreen.cs
@@ -11,6 +11,22 @@ public class TitleScreen : Node
     public override void _Ready()
     {
         GD.Print("Title screen ready");
+
+        Node globalRoot = GetNode<Node>("/root/Global");
+        if (globalRoot == null)
+        {
+            GD.Print("node not found...");
+            return;
+        }
+        GD.Print("!!! found global root!");
+        GlobalVars globalVars = globalRoot.GetNode<GlobalVars>("GlobalVars");
+        if (globalVars == null)
+        {
+            GD.Print("node vars not found...");
+            return;
+        }
+        GD.Print($"!!! found global vars! {globalVars.TestMe}");
+        //GD.Print($"global var test string: {globalNode.TestMe}");
     }
 
     private void OnStartButton()
